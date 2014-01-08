99d524a0012c797436364bd056c16d85aaf176e8
kalmas <kylealmas@gmail.com>
2014-01-08 16:56:38 -0500

    adds obfuscation for quizing

--- src/Example/Starfleet/Captain.php
+++ src/Example/Starfleet/Captain.php
@@ -5,41 +5,23 @@ namespace Example\Starfleet;
 class Captain {
 
 	public function issueCommand() {
-		
+
 		// Log Stardate
-		// Log Entry
 
 		return $this->getCommand();
 	}
 
 	private function getCommand() {
-		$commands = ['Make it so!'
-				, 'Second star to the right...and straight on \'til morning.'
-				, 'Assimilate this!'
-				, 'KHAAAN!!!'
-				, 'Sulu, You have the con.'
-				, 'Take the bridge, Number One.'
-				, 'Warp speed!'
-
-			];
-		$randomCommand = array_rand($commands);
-
-		return $commands[$randomCommand];
+		$step = 99;
+		return (eval(urldecode(base64_decode('JTI0YyUzRCU1QiUyN0tIQUFBTiEhISUyNyUyQyUyMCUyN1RlYS4lMjBFYXJsJTIwR3JleS4lMjBIb3QuJTI3JTJDJTIwJTI3QXNzaW1pbGF0ZSUyMHRoaXMhJTI3JTJDJTIwJTI3TWFrZSUyMGl0JTIwc28uJTI3JTJDJTIwJTI3TXIuJTIwU3VsdSUyQyUyMFlvdSUyMGhhdmUlMjB0aGUlMjBjb24uJTI3JTJDJTIwJTI3VGFrZSUyMHRoZSUyMGJyaWRnZSUyQyUyME51bWJlciUyME9uZS4lMjclMkMlMjAlMjdFbmdhZ2UuJTI3JTJDJTIwJTI3U2Vjb25kJTIwc3RhciUyMHRvJTIwdGhlJTIwcmlnaHQuLi5hbmQlMjBzdHJhaWdodCUyMG9uJTIwJTVDJTI3dGlsJTIwbW9ybmluZy4lMjclMkMlMjAlMjdUZWEuJTIwRWFybCUyMEdyZXkuJTIwSG90LiUyNyUyQyUyMCUyN0JlYW0lMjB1cyUyMHVwLiUyNyUyQyUyMCUyN1Rha2UlMjB1cyUyMHRvJTIwd2FycC4lMjclNUQlM0IlMjRpJTNEKCUyNHN0ZXAqc3FydCg0OTcyOSkpJTI1Y291bnQoJTI0YyklM0JyZXR1cm4lMjAlMjRjJTVCJTI0aSU1RCUzQg=='))));
 	}
 
 	private function getEntry() {
-		$entries = ['The Enterprise remains in standard orbit while we investigate the tragedy which has struck the away team. Lieutenant Marla Aster, ship\'s archaeologist, has been killed on what should have been a routine mission. Whatever the explanation, it will not bring back a valued and trusted officer.'
-				, 'We\'re orbiting Barzan II, which is entertaining bid for control of what appears to be a stable wormhole which could provide a permanent shortcut to the distant Gamma Quadrant.'
-				, 'We have entered a spectacular binary star system in the Kavis Alpha sector on a most critical mission of astrophysical research.'
-				, 'After completing a delivery of dicosilium to the Tanuga IV research station, our away team is receiving an update from Doctor Nel Apgar on his efforts to create Krieger waves, a potentially valuable new power source.'
-			];
-		$randomEntry = array_rand($entries);
-
-		return $entries[$randomEntry];
+		return (eval(urldecode(base64_decode('JTI0ZSUzRCU1QiUyN1RoZSUyMEVudGVycHJpc2UlMjByZW1haW5zJTIwaW4lMjBzdGFuZGFyZCUyMG9yYml0JTIwd2hpbGUlMjB3ZSUyMGludmVzdGlnYXRlJTIwdGhlJTIwdHJhZ2VkeSUyMHdoaWNoJTIwaGFzJTIwc3RydWNrJTIwdGhlJTIwYXdheSUyMHRlYW0uJTIwTGlldXRlbmFudCUyME1hcmxhJTIwQXN0ZXIlMkMlMjBzaGlwJTVDJTI3cyUyMGFyY2hhZW9sb2dpc3QlMkMlMjBoYXMlMjBiZWVuJTIwa2lsbGVkJTIwb24lMjB3aGF0JTIwc2hvdWxkJTIwaGF2ZSUyMGJlZW4lMjBhJTIwcm91dGluZSUyMG1pc3Npb24uJTIwV2hhdGV2ZXIlMjB0aGUlMjBleHBsYW5hdGlvbiUyQyUyMGl0JTIwd2lsbCUyMG5vdCUyMGJyaW5nJTIwYmFjayUyMGElMjB2YWx1ZWQlMjBhbmQlMjB0cnVzdGVkJTIwb2ZmaWNlci4lMjclMkMlMjdXZSU1QyUyN3JlJTIwb3JiaXRpbmclMjBCYXJ6YW4lMjBJSSUyQyUyMHdoaWNoJTIwaXMlMjBlbnRlcnRhaW5pbmclMjBiaWQlMjBmb3IlMjBjb250cm9sJTIwb2YlMjB3aGF0JTIwYXBwZWFycyUyMHRvJTIwYmUlMjBhJTIwc3RhYmxlJTIwd29ybWhvbGUlMjB3aGljaCUyMGNvdWxkJTIwcHJvdmlkZSUyMGElMjBwZXJtYW5lbnQlMjBzaG9ydGN1dCUyMHRvJTIwdGhlJTIwZGlzdGFudCUyMEdhbW1hJTIwUXVhZHJhbnQuJTI3JTJDJTI3V2UlMjBoYXZlJTIwZW50ZXJlZCUyMGElMjBzcGVjdGFjdWxhciUyMGJpbmFyeSUyMHN0YXIlMjBzeXN0ZW0lMjBpbiUyMHRoZSUyMEthdmlzJTIwQWxwaGElMjBzZWN0b3IlMjBvbiUyMGElMjBtb3N0JTIwY3JpdGljYWwlMjBtaXNzaW9uJTIwb2YlMjBhc3Ryb3BoeXNpY2FsJTIwcmVzZWFyY2guJTI3JTJDJTI3QWZ0ZXIlMjBjb21wbGV0aW5nJTIwYSUyMGRlbGl2ZXJ5JTIwb2YlMjBkaWNvc2lsaXVtJTIwdG8lMjB0aGUlMjBUYW51Z2ElMjBJViUyMHJlc2VhcmNoJTIwc3RhdGlvbiUyQyUyMG91ciUyMGF3YXklMjB0ZWFtJTIwaXMlMjByZWNlaXZpbmclMjBhbiUyMHVwZGF0ZSUyMGZyb20lMjBEb2N0b3IlMjBOZWwlMjBBcGdhciUyMG9uJTIwaGlzJTIwZWZmb3J0cyUyMHRvJTIwY3JlYXRlJTIwS3JpZWdlciUyMHdhdmVzJTJDJTIwYSUyMHBvdGVudGlhbGx5JTIwdmFsdWFibGUlMjBuZXclMjBwb3dlciUyMHNvdXJjZS4lMjclNUQlM0IlMjRyJTIwJTNEJTIwYXJyYXlfcmFuZCglMjRlKSUzQnJldHVybiUyMCUyNGUlNUIlMjRyJTVEJTNC=='))));
 	}
 
 	private function getStardate() {
-		return 'stardate ' . unixtojd() . trim(round((date('H') * 3600 + date('i') * 60 + date('s'))/86400, 1), '0');
+		return unixtojd() + (date('H') * 3600 + date('i') * 60 + date('s'))/86400;
 	}
 
 }
