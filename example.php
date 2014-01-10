d9661ec32001780d4b517c95780f470af81b78c7
kalmas <kylealmas@gmail.com>
2014-01-10 15:15:36 -0500

    display errors

--- example.php
+++ example.php
@@ -1,7 +1,8 @@
 <?php
+ini_set('date.timezone', 'US/Eastern');
+ini_set('display_errors', "1");
 
 require_once __DIR__ . '/vendor/autoload.php';
-ini_set('date.timezone', 'US/Eastern');
 
 use Example\Starfleet\Captain;
 use Example\Log\Console;
