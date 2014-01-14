814cfae6ee32e22a7c8206cc11859e37bfea01ec
kalmas <kylealmas@gmail.com>
2014-01-14 09:21:18 -0500

    adds mock object to test

--- tests/Example/Starfleet/CaptainTest.php
+++ tests/Example/Starfleet/CaptainTest.php
@@ -1,11 +1,21 @@
 <?php
 
 use Example\Starfleet\Captain;
-use Example\Log\Console;
+use Example\Log\ILog;
 class CaptainTest extends PHPUnit_Framework_TestCase {
 	
-	public function test_issueCommand_returns_string(){		
-		$cap = new Captain(new Console());
+	public function test_issueCommand_returns_string(){	
+		$mockLog = $this->getMockBuilder('Example\Log\ILog')
+			->setMethods(array('println'))
+			->getMock();
+		$mockLog->expects($this->at(0))
+			->method('println')
+			->with($this->matchesRegularExpression('/Captain\'s Log, Stardate [\d\.]+/'), $this->equalTo(true));
+		$mockLog->expects($this->at(1))
+			->method('println')
+			->with($this->isType('string'));
+		
+		$cap = new Captain($mockLog);
 		$command = $cap->issueCommand();
 		
 		$this->assertInternalType('string', $command);
